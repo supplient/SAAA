dfaf7c7967c26ad542e83122c81d675506b8e1f7
supplient <437566830@qq.com>
2025-09-05 09:30:58 +0800

    version: 更新版本到v0.0.8

--- StrategicAssetAllocationAssistant/app/build.gradle.kts
+++ StrategicAssetAllocationAssistant/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.strategicassetallocationassistant"
         minSdk = 33
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
